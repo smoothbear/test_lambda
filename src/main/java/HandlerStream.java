add5118c14ec357e706dab672b857621f30e6155
smoothbear <kjbin0420@naver.com>
2021-07-06 19:54:35 +0900

    Replace to root path

---
+++ src/main/java/HandlerStream.java
@@ -0,0 +1,23 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Map;
+
+public class HandlerStream implements RequestHandler<Map<String, String>, String> {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public String handleRequest(Map<String, String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        String response = "200 OK";
+
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+        return response;
+    }
+}
